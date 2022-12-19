a73f78955f94d16ae3d8eb61e75837bf8892e1a7
SherlockLena <slavinskaya.lena2001@gmail.com>
2022-12-20 00:14:37 +0200

    work with props

---
+++ src/components/app/app.js
@@ -0,0 +1,30 @@
+import './app.css';
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+function App() {
+
+    const data = [
+        {name: 'John S.', salary: 800, increase: false, id: 1},
+        {name: 'Alex M.', salary: 1000, increase: true, id: 2},
+        {name: 'Jack C.', salary: 1200, increase: false, id: 3},
+    ];
+
+    return(
+        <div className="app">
+            <AppInfo/>
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+            <EmployeesList data={data}/>
+            <EmployeesAddForm/>
+        </div>
+    );
+}
+
+export default App;
